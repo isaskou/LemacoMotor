d566fbe1b481c0c6157dc2a93aa8fa69d6558062
isaskou <isabel.skou@yahoo.fr>
2021-05-14 16:54:15 +0200

    add Contact page

---
+++ src/app/Pages/contact/contact-adresse/contact-adresse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-adresse',
+  templateUrl: './contact-adresse.component.html',
+  styleUrls: ['./contact-adresse.component.scss']
+})
+export class ContactAdresseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Pages/contact/contact-title/contact-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-title',
+  templateUrl: './contact-title.component.html',
+  styleUrls: ['./contact-title.component.scss']
+})
+export class ContactTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
