ffea89ebe50cdbe1f604838dcf721e60b2555102
isaskou <isabel.skou@yahoo.fr>
2021-05-14 16:49:44 +0200

    add footer & navbar

---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  navbarItem: Array<any>=[
+    {name:'home', link:'/home'},
+    {name:'services', link:'/services'},
+    {name:'parking', link:'/parking'},
+    {name:'Vente', link:'/vente'},
+    {name:'contact', link:'/contact'},
+
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
