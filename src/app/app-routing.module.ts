3a9d119b619b525b8c3c65ccb61ba48c65fcd5f2
isaskou <isabel.skou@yahoo.fr>
2021-05-14 16:58:17 +0200

    update commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ParkingComponent } from './Pages/parking/parking.component';
+import { ServicesComponent } from './Pages/services/services.component';
+import { AustinComponent } from './Pages/vente/vente-aVendre/austin/austin.component';
+import { VenteComponent } from './Pages/vente/vente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'services', component:ServicesComponent},
+  {path:'parking', component:ParkingComponent},
+  {path:'vente', component:VenteComponent},
+  {path:'contact', component:ContactComponent},
+  {path: 'austin', component:AustinComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'top',
+    scrollOffset: [0, 0],
+    anchorScrolling: "enabled",
+    initialNavigation: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,88 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PopoverModule} from 'ngx-bootstrap/popover';
+import {BootstrapIconsModule} from 'ng-bootstrap-icons';
+import {allIcons} from 'ng-bootstrap-icons/icons';
+
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ContactComponent } from './Pages/contact/contact.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { ParkingComponent } from './Pages/parking/parking.component';
+import { ServicesComponent } from './Pages/services/services.component';
+import { VenteComponent } from './Pages/vente/vente.component';
+import { HomeBannerComponent } from './Pages/home/home-banner/home-banner.component';
+import { HomeFeatureCardComponent } from './Pages/home/home-feature-card/home-feature-card.component';
+import { HomeFeaturesComponent } from './Pages/home/home-features/home-features.component';
+import { NgbCarousel, NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServicesTitleComponent } from './Pages/services/services-title/services-title.component';
+import { ParkingTitleComponent } from './Pages/parking/parking-title/parking-title.component';
+import { ParkingAccrocheComponent } from './Pages/parking/parking-accroche/parking-accroche.component';
+import { ParkingIconesComponent } from './Pages/parking/parking-icones/parking-icones.component';
+import { ParkingServiceComponent } from './Pages/parking/parking-service/parking-service.component';
+import { ContactAdresseComponent } from './Pages/contact/contact-adresse/contact-adresse.component';
+import { ContactTitleComponent } from './Pages/contact/contact-title/contact-title.component';
+import { ContactMapComponent } from './Pages/contact/contact-map/contact-map.component';
+import { VenteTitleComponent } from './Pages/vente/vente-title/vente-title.component';
+import { VenteTestAVendreComponent } from './Pages/vente/vente-test-avendre/vente-test-avendre.component';
+import { VentePreviousNextComponent } from './Pages/vente/vente-previous-next/vente-previous-next.component';
+import { AustinComponent } from './Pages/vente/vente-aVendre/austin/austin.component';
+import { BlogComponent } from './Pages/vente/blog/blog.component';
+import { BlogCardsComponent } from './Pages/vente/blog-cards/blog-cards.component';
+import { DiapoComponent } from './Pages/vente/vente-aVendre/austin/diapo/diapo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    LayoutComponent,
+    ContactComponent,
+    HomeComponent,
+    ParkingComponent,
+    ServicesComponent,
+    VenteComponent,
+    FooterComponent,
+    NavbarComponent,
+    HomeBannerComponent,
+    HomeFeatureCardComponent,
+    HomeFeaturesComponent,
+    ServicesTitleComponent,
+    ParkingTitleComponent,
+    ParkingAccrocheComponent,
+    ParkingIconesComponent,
+    ParkingServiceComponent,
+    ContactAdresseComponent,
+    ContactTitleComponent,
+    ContactMapComponent,
+    VenteTitleComponent,
+    VenteTestAVendreComponent,
+    VentePreviousNextComponent,
+    AustinComponent,
+    BlogComponent,
+    BlogCardsComponent,
+    DiapoComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ModalModule.forRoot(),
+    PopoverModule.forRoot(),
+    BrowserAnimationsModule, 
+    BootstrapIconsModule.pick(allIcons), 
+    NgbModule,
+    NgbCarouselModule
+    
+  ],
+  exports:[
+    ModalModule,
+    PopoverModule,
+    BootstrapIconsModule, 
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
