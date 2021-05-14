3fe345eca0ca757fd9808cfd59584d6030d8a494
isaskou <isabel.skou@yahoo.fr>
2021-05-14 16:52:53 +0200

    add Services page

---
+++ src/app/Pages/services/services-title/services-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-title',
+  templateUrl: './services-title.component.html',
+  styleUrls: ['./services-title.component.scss']
+})
+export class ServicesTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
