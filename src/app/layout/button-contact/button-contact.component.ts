ebed66c841272c5cde12c84c50201972272be7d2
isaskou <isabel.skou@yahoo.fr>
2021-05-16 18:56:06 +0200

    add a fixed contact button

---
+++ src/app/layout/button-contact/button-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-contact',
+  templateUrl: './button-contact.component.html',
+  styleUrls: ['./button-contact.component.scss']
+})
+export class ButtonContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
